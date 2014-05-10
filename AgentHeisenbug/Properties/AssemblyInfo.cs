c73adc5fef9ece974b0006bbd67aa1904d4f4339
Andrey Shchekin <ashmind@gmail.com>
2014-05-11 02:17:03 +1200

    Initial version of extension package.

--- AgentHeisenbug/Properties/AssemblyInfo.cs
+++ AgentHeisenbug/Properties/AssemblyInfo.cs
@@ -1,22 +1,28 @@
 using System.Reflection;
-using JetBrains.ActionManagement;
+using AgentHeisenbug.Properties;
 using JetBrains.Application.PluginSupport;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Andrey Shchekin")]
-[assembly: AssemblyProduct("AgentHeisenbug")]
-[assembly: AssemblyCopyright("Copyright © Andrey Shchekin, 2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCompany(AssemblyInfo.Author)]
+[assembly: AssemblyProduct(AssemblyInfo.Title)]
+[assembly: AssemblyCopyright("Copyright © " + AssemblyInfo.Author + ", 2014")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion(AssemblyInfo.Version)]
+[assembly: AssemblyFileVersion(AssemblyInfo.Version)]
+[assembly: AssemblyInformationalVersion(AssemblyInfo.FullVersion)]
 
 // The following information is displayed by ReSharper in the Plugins dialog
-[assembly: PluginTitle("AgentHeisenbug")]
+[assembly: PluginTitle("Agent Heisenbug")]
 [assembly: PluginDescription("")]
-[assembly: PluginVendor("Andrey Shchekin")]
+[assembly: PluginVendor(AssemblyInfo.Author)]
+
+namespace AgentHeisenbug.Properties {
+    internal static class AssemblyInfo {
+        public const string Author = "Andrey Shchekin";
+        public const string Title = "AgentHeisenbug";
+        public const string Version = "0.5.0";
+        public const string FullVersion = Version + "-pre";
+    }
+}
